91460ffd7764bec99b59c27fcd17e9e5d3aca6d2
Angelo Cigna <acigna1982@gmail.com>
2017-12-04 20:33:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,3 +8,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
+
+function removeFromPlaylist() {
+   delete playlist.artistName
+   return playlist
+}
