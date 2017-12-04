08c36bd8ef607cd3ff98f77e09384bb2697f5a23
Angelo Cigna <acigna1982@gmail.com>
2017-12-04 20:31:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,6 @@ var playlist = {
 // Object.assign playlist['Phil Ochs'] = "Here's to the State of Mississippi"
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return playlist[artistName] = songTitle}
+  playlist[artistName] = songTitle
+  return playlist
 }
