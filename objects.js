4bcbbcee0a7a593ca8e0a89e127b63a3ac03b4dc
Angelo Cigna <acigna1982@gmail.com>
2017-12-04 20:15:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
 var playlist = {
-  metallica: 'one',
-  ozzy: 'paranoid'
+  // metallica: 'one',
+  // ozzy: 'paranoid'
 }
 
-function updatePlaylist() {
-return playlist.assign(artistName: songTitle)
-}
+// function updatePlaylist() {
+//   playlist.
+// }
